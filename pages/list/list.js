3c9ff54ae2ae56353067c066abeb1205ec432b2d
wyy <weiyuyang_123@163.com>
2021-11-05 03:05:44 +0800

    完成购物车功能开发，实现商品加减，同步购物车，总价更新，商品数量更新功能

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,17 +1,28 @@
 const app = getApp()
+function itemIndexInCart(foodName, cartData){
+  for(let i = 0; i < cartData.length; i++){
+    if(foodName === cartData[i].foodName){
+      return i
+    }
+  }
+  //-1表示不存在cartData中
+  return -1
+}
 
 Page({
   distance: 0,
   heightArr: [],
   GETRPX: 0,
   data: {
-    listData:{},
-    selectIndex: 0,
-    listIndex: "l0",
-    model:"",
-    totalPrice:"0"
+    listData:{}, //总的数据，云数据库获得。
+    selectIndex: 0, //左侧menu所选的index
+    listIndex: "l0",  //右侧列表跳转的index
+    model:"", //首页传入，表明是自提还是外卖。影响地址管理
+    cartData:[], //购物车数据，提交后上传数据库
+    totalPrice:0, //总价
+    showCart:false
   },
-  //计算生产滚动切换的target距离。
+  //获取云端数据，初始化自适应比率，滚动节点高度等。
   onLoad:function(e){
     //首页传入参数，确定工作模式是现场还是外卖。决定购物车是否带地址。
     this.setData({
@@ -67,7 +78,7 @@ Page({
       let heightArr=[]
       let sum = 0
       this.data.listData.map(x => {
-        sum += x.content.length * 200 + 41
+        sum += x.content.length * 250 + 42
         heightArr.push(sum)
       })
       this.heightArr = heightArr
@@ -117,5 +128,74 @@ Page({
       url: "/pages/xiangqing/xiangqing?" + param,
     })
   },
-
+  //更新总价。在每次添加，删除操作之后调用此方法，更新总价。
+  updateTotalPrice:function(){
+    let cartData = this.data.cartData
+    let totalPrice = 0
+    for(let i = 0; i < cartData.length; i++){
+      totalPrice += cartData[i].price*cartData[i].number
+    }
+    this.setData({
+      totalPrice:totalPrice
+    })
+  },
+  addItem:function(e){
+    let item = e.currentTarget.dataset.item
+    let itemIndex = itemIndexInCart(item.foodName, this.data.cartData)
+    if(itemIndex === -1){
+      let addItem = {}
+      let cartData=this.data.cartData
+      addItem.foodName = item.foodName
+      addItem.image = item.images[0]
+      addItem.price = item.price
+      addItem.number = 1
+      cartData.push(addItem)
+      this.setData({
+        cartData:cartData
+      },()=>{
+        this.updateTotalPrice()
+      })
+    }else{
+      let number = this.data.cartData[itemIndex].number + 1
+      let updateNumber = `cartData[${itemIndex}].number`
+      this.setData({
+        [updateNumber]:number
+      },() => {
+        this.updateTotalPrice()
+      })
+    }
+  },
+  deleteItem:function(e){
+    let foodName = e.currentTarget.dataset.item
+    let cartData = this.data.cartData
+    let itemIndex = itemIndexInCart(foodName, cartData)
+    if(cartData[itemIndex].number > 1){
+      let number = cartData[itemIndex].number - 1
+      let updateNumber = `cartData[${itemIndex}].number`
+      this.setData({
+        [updateNumber]:number
+      },() => {
+        this.updateTotalPrice()
+      })
+    }else if(cartData[itemIndex].number === 1){
+      cartData.splice(itemIndex, 1)
+      this.setData({
+        cartData:cartData
+      },() => {
+        this.updateTotalPrice()
+      })
+    }else{
+      throw new Error("商品数量出错")
+    }
+  },
+  showCart:function(){
+    this.setData({
+      showCart:true
+    })
+  },
+  closeCart:function(){
+    this.setData({
+      showCart:false
+    })
+  }
 })
